0d554c50783344af6b7331cf0b10ac00094596d3
Marc Tort Pascual <marctorpas@campus.monlau.com>
2023-03-08 20:09:46 +0100

    .l.o.l

--- mt-horinzontal-sidebar.js
+++ mt-horinzontal-sidebar.js
@@ -3,7 +3,7 @@ class Horinzontalsiderbar extends HTMLElement{
         super()
 
         this.side_dom=this.attachShadow({mode:"open"})
-        this.elements=[{Tittle:'prueba',Adr:'prueba.html'}]
+        this.elements=[{Tittle:'prueba',Adr:'prueba.html'},{Tittle:'prueba2',Adr:'prueba2.html'}]
         this.css=document.createElement("link")
         this.css.setAttribute("href","mt-horinzontal-sidebar.css")
         this.css.setAttribute("rel","stylesheet")
@@ -11,21 +11,15 @@ class Horinzontalsiderbar extends HTMLElement{
     updatesidebar(){
         
         this.List=document.createElement("ul")
-        this.Listitem=document.createElement("li")
         //this.parsed=JSON.parse(this.elements)
         this.side_dom.appendChild(this.css)
         this.side_dom.appendChild(this.List)
         this.elements.forEach(function (e){
-            console.log(e)
-            this.Listitem=document.createElement("li")
-            this.Listitem.innerHTML=e.Tittle
-            this.side_dom.appendChild(this.Listitem)
-            this.adr=document.createElement("a")
-            this.adr.setAttribute("href",e.Adr)
-            this.Listitem.appendChild(this.adr)
+            
         });
+        
     }
-    connectedCallback(){   
+    connectedCallback(){     
         this.updatesidebar()
     }
 }
