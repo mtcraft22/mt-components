04338d52f76fcea2747f1746f012a361ad8346ce
Marc Tort Pascual <marctorpas@campus.monlau.com>
2023-03-10 17:58:10 +0100

    añadido sidebar2

--- mt-horinzontal-sidebar.js
+++ mt-horinzontal-sidebar.js
@@ -3,23 +3,22 @@ class Horinzontalsiderbar extends HTMLElement{
         super()
 
         this.side_dom=this.attachShadow({mode:"open"})
-        this.elements=[{Tittle:'prueba',Adr:'prueba.html'},{Tittle:'prueba2',Adr:'prueba2.html'}]
+        this.elements=[{Tittle:'prueba',Adr:'prueba.html'}]
         this.css=document.createElement("link")
         this.css.setAttribute("href","mt-horinzontal-sidebar.css")
         this.css.setAttribute("rel","stylesheet")
     }
     updatesidebar(){
-        
         this.List=document.createElement("ul")
         //this.parsed=JSON.parse(this.elements)
         this.side_dom.appendChild(this.css)
         this.side_dom.appendChild(this.List)
         for (let i=0;i<this.elements.length;i++){
             this.adress=document.createElement("a")
-            this.adress.setAttribute("href",)
+            this.adress.setAttribute("href",this.elements[i].Adr)
             this.listitem=document.createElement("li")
+            this.adress.innerHTML=this.elements[i].Tittle
             this.listitem.append(this.adress)
-            this.listitem.innerHTML=this.elements[i].Tittle
             this.List.appendChild(this.listitem)
         }
     }
