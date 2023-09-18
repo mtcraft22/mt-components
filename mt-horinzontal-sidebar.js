d212ebb84c39483fd205fd7d72b80b786231be5d
mtcraft22 <97896526+mtcraft22@users.noreply.github.com>
2023-09-18 15:37:37 +0200

    Update mt-horinzontal-sidebar.js

--- mt-horinzontal-sidebar.js
+++ mt-horinzontal-sidebar.js
@@ -1,56 +1,61 @@
-
-class Horinzontalsiderbar extends HTMLElement{
-    constructor(){
-        super()
-        this.cssroute="./css/sidebar.css"
-        this.side_dom=this.attachShadow({mode:"open"})
-        this.elements=[{Tittle:'prueba',Adr:'prueba.html'}]
-        this.css=document.createElement("link")
-        this.css.setAttribute("href",cssroute)
-        this.css.setAttribute("rel","stylesheet")
-    }
-    static get observedAttributes(){
-        return ["items","css"]
-    }
-    updatesidebar(){
-        var fileName = location.href.split("/").slice(-1)
-        this.List=document.createElement("ul")
-        this.List.setAttribute("id","sidebar")
-        //this.parsed=JSON.parse(this.elements)
-        this.side_dom.appendChild(this.css)
-        this.side_dom.appendChild(this.List)
-        for (let i=0;i<this.elements.length;i++){
-            this.adress=document.createElement("a")
-            this.adress.setAttribute("href",this.elements[i].Adr)
-            
-            this.listitem=document.createElement("li")
-            if (this.elements[i].Adr==fileName){this.listitem.setAttribute("class","selected")}
-            this.adress.innerHTML=this.elements[i].Tittle
-            this.listitem.append(this.adress)
-            this.List.appendChild(this.listitem)
-        }
-    }
-    connectedCallback(){
-        if(this.side_dom.getElementById("sidebar")!=null){
-            this.side_dom.getElementById("sidebar").remove()
-        }     
-        this.updatesidebar()
-    }
-    attributeChangedCallback(nombre,viejovalor,nuevovalor){
-        if (nombre=="items"){
-            this.elements=[]
-            let lista=Array(nuevovalor)
-            this.elements=JSON.parse(lista)
-            if(this.side_dom.getElementById("sidebar")!=null){
-                this.side_dom.getElementById("sidebar").remove()
+ class Horinzontalsiderbar extends HTMLElement{
+            constructor(){
+                super()
+                this.cssroute=""
+                this.side_dom=this.attachShadow({mode:"open"})
+                this.elements=[{Tittle:'prueba',Adr:'prueba.html'}]
+                this.css=document.createElement("link")
+                this.css.setAttribute("href",this.cssroute)
+                this.css.setAttribute("rel","stylesheet")
+                this.css.setAttribute("id","link2css")
             }
-            this.updatesidebar()
+            static get observedAttributes(){
+                return ["items","css"]
+            }
+            updatesidebar(){
+                var fileName = location.href.split("/").slice(-1)
+                this.List=document.createElement("ul")
+                this.List.setAttribute("id","sidebar")
+                //this.parsed=JSON.parse(this.elements)
+                this.side_dom.appendChild(this.css)
+                this.side_dom.appendChild(this.List)
+                for (let i=0;i<this.elements.length;i++){
+                    this.adress=document.createElement("a")
+                    this.adress.setAttribute("href",this.elements[i].Adr)
+                    
+                    this.listitem=document.createElement("li")
+                    if (this.elements[i].Adr==fileName){this.listitem.setAttribute("class","selected")}
+                    this.adress.innerHTML=this.elements[i].Tittle
+                    this.listitem.append(this.adress)
+                    this.List.appendChild(this.listitem)
+                }
+            }
+            connectedCallback(){
+                if(this.side_dom.getElementById("sidebar")!=null){
+                    this.side_dom.getElementById("sidebar").remove()
+                }     
+                this.updatesidebar()
+            }
+            attributeChangedCallback(nombre,viejovalor,nuevovalor){
+                if (nombre=="items"){
+                    this.elements=[]
+                    let lista=Array(nuevovalor)
+                    this.elements=JSON.parse(lista)
+                    if(this.side_dom.getElementById("sidebar")!=null){
+                        this.side_dom.getElementById("sidebar").remove()
+                    }
+                    this.updatesidebar()
+                }
+                if (nombre=="css"){
+                    this.cssroute=nuevovalor
+                    if(this.side_dom.getElementById("link2css")!=null){
+                        this.side_dom.getElementById("link2css").remove()
+                    }
+                    this.css=document.createElement("link")
+                    this.css.setAttribute("href",this.cssroute)
+                    this.css.setAttribute("rel","stylesheet")
+                }
+            }
+        
         }
-        if (nombre=="css"){
-            this.cssroute=nuevovalor
-            this.updatesidebar()
-        }
-    }
-
-}
-customElements.define("mt-vsiderbar",Horinzontalsiderbar)
+        customElements.define("mt-vsiderbar",Horinzontalsiderbar)
