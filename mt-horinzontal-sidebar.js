2990d03970c0585cb8ee96e3544d5bbfab61424c
mtcraft22 <97896526+mtcraft22@users.noreply.github.com>
2023-10-26 10:40:22 +0200

    Update mt-horinzontal-sidebar.js

--- mt-horinzontal-sidebar.js
+++ mt-horinzontal-sidebar.js
@@ -1,4 +1,13 @@
- class Horinzontalsiderbar extends HTMLElement{
+/*
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+*/
+
+class Horinzontalsiderbar extends HTMLElement{
             constructor(){
                 super()
                 this.cssroute=""
